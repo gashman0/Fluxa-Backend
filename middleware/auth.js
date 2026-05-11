554d972683d4421126d95facc5813c5c140a71cc
gashman0 <gashman878@gmail.com>
2026-05-11 22:01:29 +0100

    Added console.log to middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,13 @@ import jwt from 'jsonwebtoken';
 
 
 export const protect = (req, res, next) => {
+
+    console.log("Protect middleware hit");
+    console.log("Cookies:", req.cookies);
+
     const token = req.cookies.accessToken;
+
+    console.log("Access Token:", token);
 
     if(!token) {
         return res.status(401).json({
@@ -12,9 +18,14 @@ export const protect = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log("Decoded User:", decoded);
+
         req.user = decoded;
         next();
     } catch (error) {
+
+        console.log("JWT Verify Error:", error.message);
         res.status(401).josn({message: "Token expired"});
     }
 }
