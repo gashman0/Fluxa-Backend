2de1f14fd0535a50a828abc2b9ef38bd086c8d47
gashman0 <gashman878@gmail.com>
2026-09-06 23:52:02 +0100

    Added Otp controller

--- routes/admin/auth-routes.js
+++ routes/admin/auth-routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { adminLogin } from "../../controllers/admin-auth/login-controller.js";
+import { verifyOtp } from "../../controllers/admin-auth/otp-controller.js";
 
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.post("/otp", adminRouter);
 
 export default adminRouter;
