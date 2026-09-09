627ca95a1958c1ed65c05cb57797426bcbcbcb17
gashman0 <gashman878@gmail.com>
2026-09-09 13:27:54 +0100

    Login/OTP issues

--- routes/user/auth-routes.js
+++ routes/user/auth-routes.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { protect } from "../middleware/auth.js";
-import { signup } from "../controllers/auth/signup-controller.js";
-import { login } from "../controllers/auth/login-controller.js";
-import { refresh } from "../controllers/auth/refresh-controller.js";
-import { logout } from "../controllers/auth/logout-controller.js";
-import { getMe } from "../controllers/user/me.js";
+import { protect } from "../../middleware/auth.js";
+import { signup } from "../../controllers/auth/signup-controller.js";
+import { login } from "../../controllers/auth/login-controller.js";
+import { refresh } from "../../controllers/auth/refresh-controller.js";
+import { logout } from "../../controllers/auth/logout-controller.js";
+import { getMe } from "../../controllers/user/me.js";
 
 // import { getJobs } from "../controllers/job/jobs-controller.js";
-
 
 const authRouter = express.Router();
 
