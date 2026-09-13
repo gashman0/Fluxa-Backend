ac7f31be31965b3fe1bd6422cd2683d959b236c3
gashman0 <gashman878@gmail.com>
2026-09-13 01:38:25 +0100

    Fixed admin refresh token

--- routes/admin/auth-routes.js
+++ routes/admin/auth-routes.js
@@ -4,11 +4,13 @@ import { adminLogout } from "../../controllers/admin-auth/logout-controller.js";
 import { verifyOtp } from "../../controllers/admin-auth/otp-controller.js";
 import { getMeAdmin } from "../../controllers/user/adminMe-controller.js";
 import { protectAdmin } from "../../middleware/protectAdmin.js";
+import { refresh } from "../../controllers/admin-auth/refresh-controller.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
 adminRouter.post("/otp", verifyOtp);
+adminRouter.post(".refresh", refresh);
 adminRouter.post("/logout", adminLogout); 
 
 
